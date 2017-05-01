a109fbbd62771ce9de4d1152124a59de32b9d9ea
Jungne <jungne7@gmail.com>
2017-05-01 11:22:49 +0200

    Tilføjet/oprettet nogle klasser/filer
    Oprettet productFile.txt.
    Oprettet FileSearcher klassen, til at læse filer og søge igennem dem.
    Tilføjet attributter til Product.
    Oprettet ProductCategory.

---
+++ src/Webshop/FileSearcher.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Webshop;
+
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class FileSearcher {
+    
+    public static TreeSet<Product> findProduct(String query) {
+        TreeSet<Product> productSet = new TreeSet<>();
+        Scanner scanner = new Scanner("productFile.txt");
+        
+        return productSet;
+    }
+}
--- src/Webshop/Product.java
+++ src/Webshop/Product.java
@@ -1,5 +1,26 @@
 package Webshop;
 
 public class Product {
+    private int id;
+    private ProductCategory category;
+    private String description;
 
+    public Product(int id, ProductCategory category, String description) {
+        this.id = id;
+        this.category = category;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public ProductCategory getCategory() {
+        return category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    
 }
