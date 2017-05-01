7702cf66b1ab3ea569a60535461f39f64000975e
Jungne <jungne7@gmail.com>
2017-05-01 13:59:23 +0200

    JavaFX småting

--- src/semesterprojekt/f17/FXMLDocumentController.java
+++ src/semesterprojekt/f17/FXMLDocumentController.java
@@ -1,7 +1,11 @@
 package semesterprojekt.f17;
 
+import Webshop.WebshopController;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -9,17 +13,24 @@ import javafx.scene.control.Label;
 
 public class FXMLDocumentController implements Initializable {
 
-	@FXML
-	private Label label;
+    private WebshopController webshopController;
+    
+    @FXML
+    private Label label;
 
-	@FXML
-	private void handleButtonAction(ActionEvent event) {
-		System.out.println("You clicked me!");
-		label.setText("Hello World!");
-	}
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
 
-	@Override
-	public void initialize(URL url, ResourceBundle rb) {
-		//TODO
-	}
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        try {
+            webshopController = new WebshopController();
+        } catch (IOException ex) {
+            //Do something about this.
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
