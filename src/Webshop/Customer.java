481c569f3132f64d606a91d72ed2ee33118da08c
Nekaerth <lasse.j.lundin@gmail.com>
2017-05-01 11:18:30 +0200

    Set up Custommer. Added getter/setter

--- src/Webshop/Customer.java
+++ src/Webshop/Customer.java
@@ -2,4 +2,47 @@ package Webshop;
 
 public class Customer {
 
+	private int id;
+	private String name;
+	private String email;
+	private int phoneNumber;
+	private ShoppingBasket shoppingBasket;
+
+	public Customer(String name, String email, int phoneNumber) {
+		this.name = name;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getPhoneNumber() {
+		return this.phoneNumber;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public ShoppingBasket getShoppingBasket() {
+		return this.shoppingBasket;
+	}
 }
