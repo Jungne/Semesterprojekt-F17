ac17649c1a3165c6daca1fb58c25124fa7ea9dd6
Nekaerth <lasse.j.lundin@gmail.com>
2017-05-01 10:55:16 +0200

    Minor fix

--- src/Webshop/Catalog.java
+++ src/Webshop/Catalog.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Webshop;
 
-import java.util.List;
-
 public class Catalog {
-    
-    private List list;
-    
+
 }
