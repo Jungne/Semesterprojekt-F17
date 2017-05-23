e7689160f363a59b03732c5251536a0629783202
Hjalte Fromholt Rindom <hjaltefromholtrindom@10.126.54.8>
2017-05-23 10:14:25 +0200

    get images interface returtype results

--- src/Webshop/DatabaseInterface.java
+++ src/Webshop/DatabaseInterface.java
@@ -1,5 +1,10 @@
 package Webshop;
 
+<<<<<<< Updated upstream
+=======
+import java.io.File;
+import java.sql.ResultSet;
+>>>>>>> Stashed changes
 import java.util.ArrayList;
 import java.util.TreeSet;
 import javafx.scene.image.Image;
@@ -21,7 +26,12 @@ public interface DatabaseInterface {
 	public boolean saveOrder(Order order);
 
 	public void addImage(String imagePath, String title, int category);
+<<<<<<< Updated upstream
 
 	public Image getImage();
+=======
+	
+	public ResultSet getImages();
+>>>>>>> Stashed changes
 
 }
