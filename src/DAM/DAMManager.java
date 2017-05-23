7da0387384deaebdb6b4a5cef21b041e13b5eaa4
Hjalte Fromholt Rindom <hjaltefromholtrindom@10.126.54.8>
2017-05-23 10:33:02 +0200

    getImages returtype rettet

--- src/DAM/DAMManager.java
+++ src/DAM/DAMManager.java
@@ -2,21 +2,22 @@ package DAM;
 
 import DBManager.DBManager;
 import Webshop.DatabaseInterface;
+import java.sql.ResultSet;
 import javafx.scene.image.Image;
 
 public class DAMManager {
 
-	DatabaseInterface databaseInterface;
+    DatabaseInterface databaseInterface;
 
-	public DAMManager() {
-		databaseInterface = DBManager.getInstance();
-	}
+    public DAMManager() {
+        databaseInterface = DBManager.getInstance();
+    }
 
-	public void addImage(String imagePath, String title, int category) {
-		databaseInterface.addImage(imagePath, title, category);
-	}
+    public void addImage(String imagePath, String title, int category) {
+        databaseInterface.addImage(imagePath, title, category);
+    }
 
-	public Image getImage() {
-		return databaseInterface.getImage();
-	}
+    public ResultSet getImage() {
+        return databaseInterface.getImages();
+    }
 }
--- src/Webshop/DatabaseInterface.java
+++ src/Webshop/DatabaseInterface.java
@@ -1,37 +1,28 @@
 package Webshop;
 
-<<<<<<< Updated upstream
-=======
-import java.io.File;
 import java.sql.ResultSet;
->>>>>>> Stashed changes
+
 import java.util.ArrayList;
 import java.util.TreeSet;
-import javafx.scene.image.Image;
 
 public interface DatabaseInterface {
 
-	//Methods for Catalog:
-	public Product getProduct(int productId);
+    //Methods for Catalog:
+    public Product getProduct(int productId);
 
-	public ArrayList<Product> getAllProducts();
+    public ArrayList<Product> getAllProducts();
 
-	public ArrayList<Product> findProducts(String query);
+    public ArrayList<Product> findProducts(String query);
 
-	public TreeSet<String> getCategories();
+    public TreeSet<String> getCategories();
 
-	public ArrayList<Product> getCategory(String category);
+    public ArrayList<Product> getCategory(String category);
 
-	//Methods for OrderHistory:
-	public boolean saveOrder(Order order);
+    //Methods for OrderHistory:
+    public boolean saveOrder(Order order);
 
-	public void addImage(String imagePath, String title, int category);
-<<<<<<< Updated upstream
+    public void addImage(String imagePath, String title, int category);
 
-	public Image getImage();
-=======
-	
-	public ResultSet getImages();
->>>>>>> Stashed changes
+    public ResultSet getImages();
 
 }
