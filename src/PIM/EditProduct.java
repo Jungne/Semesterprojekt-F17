a80841c3b7546f1a8cb1e74cdc5796689943cbbe
Hjalte Fromholt Rindom <hjaltefromholtrindom@10.126.54.8>
2017-05-24 20:15:18 +0200

    newImage method

--- src/PIM/EditProduct.java
+++ src/PIM/EditProduct.java
@@ -9,8 +9,8 @@ import javafx.scene.image.Image;
  */
 public class EditProduct {
 
-    public void newProduct(int id, int category, double price, String description, String titel, ArrayList<Image> imageList) {
-        PIMProduct product = new PIMProduct(id, category, price, description, titel, imageList);
+    public void newProduct(int category, double price, String description, String titel, ArrayList<Integer> imageList) {
+        
     }
 
     public void editProduct(int id) {
