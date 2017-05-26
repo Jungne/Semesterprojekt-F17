5eafa5ba7a147675bd40020dff4c79012969052d
Hjalte Fromholt Rindom <hjaltefromholtrindom@10.126.54.8>
2017-05-26 10:41:19 +0200

    getAllProducts in PIM done

--- src/PIM/PIMProduct.java
+++ src/PIM/PIMProduct.java
@@ -10,19 +10,19 @@ import javafx.scene.image.Image;
 public class PIMProduct {
     
     private int id;
-    private int category;
+    private String category;
     private double price;
     private String description;
-    private String titel;
-    private ArrayList<Image> imageList;
+    private String name;
+    private ArrayList<Image> images;
     
-    public PIMProduct(int id, int category, double price, String description, String titel, ArrayList<Image> imageList) {
+    public PIMProduct(String name, int id, String category, String description, double price, ArrayList<Image> images) {
         this.id = id;
         this.category = category;
         this.price = price;
         this.description = description;
-        this.titel = titel;
-        this.imageList = imageList;
+        this.name = name;
+        this.images = images;
     }
 
 }
--- src/PIM/ProductSearch.java
+++ src/PIM/ProductSearch.java
@@ -1,10 +1,13 @@
 package PIM;
 
+import DBManager.DatabaseInterface;
+import Webshop.Product;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.scene.image.Image;
 
 /**
  *
@@ -12,20 +15,24 @@ import java.util.logging.Logger;
  */
 public class ProductSearch {
 
+    private static DatabaseInterface databaseInterface;
+
     public static ArrayList<PIMProduct> getAllProducts() {
 
         ArrayList<PIMProduct> productList = new ArrayList<PIMProduct>();
-        
+
         ResultSet set = DBManager.DBManager.getInstance().getAllProducts();
-        
+
         try {
-            while(set.next()) {
-                //productList.add(new PIMProduct(set.getInt(2),));
+            while (set.next()) {
+
+                int productID = set.getInt(2);
+                ArrayList<Image> images = databaseInterface.getImages(productID);
+                productList.add(new PIMProduct(set.getString(1), productID, set.getString(3), set.getString(4), set.getDouble(5), images));
             }
         } catch (SQLException ex) {
             Logger.getLogger(ProductSearch.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
 
         return productList;
     }
