c2fb64f4e4601b269907bd42ddb096a55378f18f
Hjalte Fromholt Rindom <hjaltefromholtrindom@10.126.54.8>
2017-05-26 10:19:01 +0200

    PIM product search unfinished

--- src/PIM/ProductSearch.java
+++ src/PIM/ProductSearch.java
@@ -1,6 +1,10 @@
 package PIM;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -12,6 +16,15 @@ public class ProductSearch {
 
         ArrayList<PIMProduct> productList = new ArrayList<PIMProduct>();
         
+        ResultSet set = DBManager.DBManager.getInstance().getAllProducts();
+        
+        try {
+            while(set.next()) {
+                //productList.add(new PIMProduct(set.getInt(2),));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductSearch.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
 
         return productList;
