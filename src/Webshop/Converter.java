109e27dc09caab9e4d96c89991714019a8574950
Nekaerth <lasse.j.lundin@gmail.com>
2017-05-29 13:06:40 +0200

    Added Converter.java

---
+++ src/Webshop/Converter.java
@@ -0,0 +1,23 @@
+package Webshop;
+
+import java.util.HashMap;
+
+public class Converter {
+
+	public static Customer toCustomer(HashMap<String, String> customerMap) {
+		return null; //TODO
+	}
+
+	public static Order toOrder(HashMap<String, String> orderMap) {
+		return null; //TODO
+	}
+
+	public static Product toProduct(HashMap<String, String> productMap) {
+		return null; //TODO
+	}
+
+	public static ShoppingBasket toShoppingBasket(HashMap<String, String> basketMap) {
+		return null; //TODO
+	}
+
+}
